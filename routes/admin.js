aa3d698ae3fb6ca96ab63d3face52c39dc36bf09
mahume <michael.a.hume@gmail.com>
2020-01-13 22:27:11 -0700

    Basic dummy styling and html setup

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(`
-    <form action="/add-product" method="POST">
-      <input type="text" name="title">
-      <button type="submit">Submit</button>
-    </form>
-  `);
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 
 const adminRoutes = require("./routes/admin");
@@ -10,13 +11,17 @@ const PORT = process.env.PORT || 3000;
 // Parse req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 // ERROR HANDLERS
-app.use((req, res, next) => res.status(404).send("<h1>Page not found</h1>"));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 // LISTEN
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
