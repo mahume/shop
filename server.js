6fe7e10dd06c03b0c1a4d31cdeb95eb83080d339
mahume <michael.a.hume@gmail.com>
2020-01-13 23:45:07 -0700

    Refactor code for EJS

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const express = require("express");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// TEMPLATING ENGINE
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 // MIDDELWARES
 // Parse req.body
@@ -15,13 +16,15 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES
-app.use("/admin", adminRoutes);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 // ERROR HANDLERS
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
-// LISTEN
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
